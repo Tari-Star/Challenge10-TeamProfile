701b5d84c8c3bb052f5f54b21bdb1076c4939397
Tari <bota.maral@gmail.com>
2021-10-25 21:12:57 -0400

    add manager function

--- index.js
+++ index.js
@@ -1,5 +1,69 @@
+// node modules
 const inquirer = require('inquirer');
 
+// team profiles
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+
+// team arrey
+const teamArray = [];
+
+const addManager = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter manager's name",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter manager's name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Enter manager's ID",
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    return false;
+                } else {
+                    console.log("Please enter manager's ID!");
+                    return true;
+                }
+            }  
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Enter manager's email",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log("Please enter manager's email!");
+                    return false;
+                }
+            } 
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Enter manager's office number",
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    return false;
+                } else {
+                    console.log("Please enter manager's office number!");
+                    return true;
+                }
+            }
+        }
+    ])
+}
